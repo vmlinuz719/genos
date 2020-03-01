89568eec2df3aed27cd4a8cf89fd42e39daea26c
Dave Levine <dlevine9@umd.edu>
2020-03-01 18:07:49 -0500

    added int2hex

--- src/krnl386/kmain.c
+++ src/krnl386/kmain.c
@@ -16,6 +16,22 @@
 
 #define OSVERSION "GenOS a03022020"
 
+void int2Hex(uint64_t x, char *buf, size_t size) {
+	// expect buf to be n characters long - last one gets \0
+	buf[size - 1] = '\0';
+
+	uint64_t num = x;
+	int i = size - 1;
+
+	// (void)x;
+	do {
+		buf[--i] = "0123456789ABCDEF"[num % 16];
+		num >>= 4;
+	} while (num > 0 && i);
+
+	while (--i >= 0) buf[i] = '0';
+}
+
 void kmain(multiboot_info_t *mbd, unsigned int magic) {
 	termInit();
 	termDisableCursor();
@@ -23,13 +39,27 @@ void kmain(multiboot_info_t *mbd, unsigned int magic) {
 	termPrint("\n");
 	termPrint("Copyright 2020 vmlinuz719. All rights reserved.\n\n");
 
-	(void)mbd;
+	// (void)mbd;
+	
+	char magicBuf[9];
+
 	if (magic == MULTIBOOT_BOOTLOADER_MAGIC) {
-		termPrint("Got multiboot info (0x2BADB002)\n");
+		termPrint("Got multiboot info (0x");
+		int2Hex(magic, magicBuf, 9);
+		termPrint(magicBuf);
+		termPrint(")\n");
 	} else {
 		termSetColor(vgaEntColor(vgaLRed, vgaBlue));
 		termPrint
 		("FATAL: GenOS requires a Multiboot-compliant bootloader!\n");
+	}
+
+	bool mmapValid = mbd->flags & MULTIBOOT_INFO_MEM_MAP;
+	if (mmapValid) {
+		termPrint("Multiboot memory map is complete (0x");
+		int2Hex(mbd->flags, magicBuf, 9);
+		termPrint(magicBuf);
+		termPrint(")\n");
 	}
 
 	termSetColor(vgaEntColor(vgaLGreen, vgaBlue));
