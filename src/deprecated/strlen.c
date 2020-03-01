755e3dff0b376dfa72990c31bb11c79ff069f2f0
vmlinuz719 <61648059+vmlinuz719@users.noreply.github.com>
2020-03-01 18:39:46 -0500

    Update strlen.c

--- src/deprecated/strlen.c
+++ src/deprecated/strlen.c
@@ -6,3 +6,18 @@ size_t deprecatedStrlen(const char* str) {
 	return len;
 }
 
+void int2Hex(uint64_t x, char *buf, size_t size) {
+	// expect buf to be n characters long - last one gets \0
+	buf[size - 1] = '\0';
+
+	uint64_t num = x;
+	int i = size - 1;
+
+	// (void)x;
+	do {
+		buf[--i] = "0123456789ABCDEF"[num % 16];
+		num >>= 4;
+	} while (num > 0 && i);
+
+	while (--i >= 0) buf[i] = '0';
+}
